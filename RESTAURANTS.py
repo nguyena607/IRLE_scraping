ae3dfc9b8331cdf2c317c6a13f0d5fcdedd36b9f
Segun Ajibola <nguyena607@berkeley.edu>
2024-05-15 14:30:55 -0700

    add folders

--- RESTAURANTS.py
+++
@@ -1,9 +0,0 @@
-RESTAURANTS = ["McDonald", 
-               "Jack in the Box", 
-               "Wendy", 
-               "Burger King", 
-               "Shake Shack",
-               "Sonic",
-               "Five Guys",
-               "Carls Jr",
-               "The Habit"]
