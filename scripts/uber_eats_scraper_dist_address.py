9c7a53c3f6b9a48d652dcbce87eab56f58d2387a
Segun Ajibola <nguyena607@berkeley.edu>
2024-06-09 21:42:43 -0700

    rescrape and re process

--- scripts/uber_eats_scraper_dist_address.py
+++ scripts/uber_eats_scraper_dist_address.py
@@ -12,7 +12,7 @@ import time
 import datetime
 import re
 from selenium.webdriver.common.action_chains import ActionChains
-from MISSING_HARDEES_LOCATIONS import LOCATIONS
+# import LOCATIONS
 from FFLOCALRESTAURANTS import RESTAURANTS
 
 
@@ -20,73 +20,38 @@ from FFLOCALRESTAURANTS import RESTAURANTS
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-LOCATIONS = [
-  '544 east ogden avenue east pointe commons ste 500, milwaukee, wi, 53202, us',
-  '3155 s padre island dr, corpus christi, tx, 78415, us',
-  '10210 berkeley place drive, 120, charlotte, nc, 28262, us',
-  '9489 westport rd, louisville, ky, 40241, us',
-  '1031 assembly st, columbia, sc, 29201, us',
-  '1901 s 10th st, mcallen, tx, 78503, us',
-  '4535 highway 58, chattanooga, tn, 37416, us',
-  '933 north central expressway, plano, tx, 75075, us',
-  '2901 airline dr, houston, tx, 77009, us',
-  '3801 s 27th street, milwaukee, wi, 53221, us',
-  '737 n. main street, layton, ut, 84041, us',
-  '2001 state road, lancaster, pa, 17601, us',
-  '4301 market st, philadelphia, pa, 19104, us',
-  '1821 n pointe drive, durham, nc, 27705, us',
-  '2727 highway 280, birmingham, al, 35223, us',
-  '1627 university ave, lubbock, tx, 79401, us',
-  '515 s henderson st, fort worth, tx, 76104, us',
-  '2815 ne green oaks blvd, grand prairie, tx, 75050, us',
-  '2015 antonio street, anthony, tx, 79821, us',
-  '1533 n. peoria, tulsa, ok, 74106, us',
-  '735 s. 11th street, beaumont, tx, 77701, us',
-  '3426 main st ste 102, pearland, tx, 77581, us',
-  '13211 hwy 90, boutte, la, 70039, us',
-  '2212 e main st, league city, tx, 77573, us',
-  '2011 north hillfield road, layton, ut, 84041, us',
-  '800 forrest st nw, atlanta, ga, 30318, us',
-  '7020 coit road, plano, tx, 75025, us',
-  '70 s aspen ave, new deal, tx, 79350, us',
-  '2414 mac davis lane, lubbock, tx, 79401, us',
-  '3112 e berry st, fort worth, tx, 76119, us',
-  '2214 n collins st, arlington, tx, 76011, us',
-  '2825 west nolana avenue, suite 10, mcallen, tx, 78504, us',
-  '1109 university ave, lubbock, tx, 79401, us',
-  '6351 e. 82nd street, indianapolis, in, 46250, us',
-  '2940 gulf freeway, league city, tx, 77573, us',
-  '5850 gulf fwy, houston, tx, 77023, us',
-  '201 e franklin st, chapel hill, nc, 27514, us',
-  '3415 navigation blvd, houston, tx, 77003, us',
-  '7221 e. admiral, tulsa, ok, 74115, us',
-  '1503 w, league city, tx, 77573, us',
-  '700 w division st, arlington, tx, 76012, us',
-  '3701 west fwy, fort worth, tx, 76107, us',
-  '2200 airport fwy ste 500-a, bedford, tx, 76022, us',
-  '19212 gulf fwy, friendswood, tx, 77546, us',
-  '3957 cottage hill road, mobile, al, 36693, us',
-  '600 east ogden avenue, milwaukee, wi, 53202, us',
-  '4656 browns hill rd, pittsburgh, pa, 15217, us',
-  '1726 bluff ridge drive, syracuse, ut, 84075, us',
-  '1702 19th st, lubbock, tx, 79401, us',
-  '1400 nasa rd. 1, nassau bay, tx, 77058, us',
-  '1821 n pointe dr, durham, nc, 27705, us',
-  '1037 chestnut st, philadelphia, pa, 19107, us',
-  '4840 s yale ave, tulsa, ok, 74135, us',
-  '1419 s 10th street, mcallen, tx, 78501, us',
-  '1313 tilghman st, allentown, pa, 18102, us',
-  '1621 middle tennessee boulevard, murfreesboro, tn, 37130, us',
-  '2514 sunset blvd, west columbia, sc, 29169, us',
-  '94 e. crosstimbers st, houston, tx, 77022, us',
-  '4200 south freeway, fort worth, tx, 76115, us',
-  '1700 s 10th street 105, mcallen, tx, 78503, us',
-  '1705 w. 21st street north, wichita, ks, 67203, us',
-  '1702 memorial blvd, murfreesboro, tn, 37129, us',
-  '1450 kenneth road, york, pa, 17408, us']
+LOCATIONS = ['2800 oxford dr, bethel park, pa, 15102, us',
+  '15190 crossroads pkwy, gulfport, ms, 39503, us',
+  '6840 northlake mall drive, charlotte, nc, 28216, us',
+  '1835 n highway 17, mount pleasant, sc, 29464, us',
+  '301 rocky run pkwy, wilmington, de, 19803, us',
+  '1100 timber dr e, garner, nc, 27529, us',
+  '240 mall blvd, monroeville, pa, 15146, us',
+  '2871 stonecrest cir, lithonia, ga, 30038, us',
+  '1114 woodruff road, greenville, sc, 29607, us',
+  '20430 highway 59 n, humble, tx, us, us',
+  '3502 e 86th st, indianapolis, in, 46240, us',
+  '3202 s highway 17, murrells inlet, sc, 29576, us',
+  '1516 s willow st, manchester, nh, 03103, us',
+  '2794 parkway, pigeon forge, tn, 37863, us',
+  '6915 w 38th st, indianapolis, in, 46214, us',
+  '3700 towne crossing blvd, mesquite, tx, 75150, us',
+  '12811 s tryon st, charlotte, nc, us, us',
+  '703 41, schererville, in, 46375, us',
+  '395 s cedar crest blvd, allentown, pa, 18103, us',
+  '3670 camp creek parkway, atlanta, ga, 30331, us',
+  '449 opry mills dr, nashville, tn, 37214, us',
+  '4951 belt line rd, dallas, tx, 75001, us',
+  '4239 nw expressway st, oklahoma city, ok, 73116, us',
+  '4423 w wendover ave, greensboro, nc, 27407, us',
+  '409 w wt harris blvd, charlotte, nc, 28262, us',
+  '311 north clark road, cedar hill, tx, 75104, us',
+  '115 hendersonvile rd, asheville, nc, 28803, us',
+  '4000 city ave, philadelphia, pa, 19131, us',
+  '2502 e springs dr, madison, wi, 53704, us']
 
-RESTAURANTS = ['Wendy'] 
-FILE_PATH = f"missing_nonca_rnd_3.jsonl"
+RESTAURANTS = ["TGI Fridays"] 
+FILE_PATH = f"missing_nonca_fullserv_rnd3.jsonl"
 # Use your own executable_path (download from https://chromedriver.chromium.org/).
 CHROMEDRIVER_PATH = "/Users/alyssanguyen/Downloads/chromedriver-mac-arm64/chromedriver"
 #CHROMEDRIVER_PATH = "/Users/sakshikolli/Downloads/chromedriver-mac-x64/chromedriver"
@@ -142,14 +107,13 @@ def find_search_box_and_enter_query(driver, query, restaurant_or_location = "RES
         return True
     except:
         return False
-  
-#Scrapes the restaurant data 
-def scrape_restaurant_data(driver, restaurant_name, location, carousel = False):
 
+def scrape_restaurant_data(driver, restaurant_name, location, carousel=False):
     item_data = {}
     wait = WebDriverWait(driver, 3)
+    
     if not carousel:
-        if find_search_box_and_enter_query(driver, restaurant_name, restaurant_or_location = "RESTAURANT"):
+        if find_search_box_and_enter_query(driver, restaurant_name, restaurant_or_location="RESTAURANT"):
             try:
                 # Click on the first restaurant result
                 first_result = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@data-testid='store-card']")))
@@ -160,6 +124,7 @@ def scrape_restaurant_data(driver, restaurant_name, location, carousel = False):
             except:
                 logger.info(f"{restaurant_name} not found at {location}.")
                 return
+            
             try:
                 # Check for and click the close button if it exists
                 close_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@data-testid='close-button']")))
@@ -167,16 +132,25 @@ def scrape_restaurant_data(driver, restaurant_name, location, carousel = False):
                 logger.info("Closed the pop-up window.")
             except:
                 logger.info("No pop-up window to close.")
+            
+            try:
+                # Check for and click the close button with the specified attributes
+                close_button_alt = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@data-baseweb='button' and @aria-label='Close' and @title='Close']")))
+                close_button_alt.click()
+                logger.info("Closed the pop-up window with the alternate close button.")
+            except:
+                logger.info("No alternate pop-up window to close or unable to find the alternate close button.")
+    
     try:
         # Find the script tag that contains the JSON data
         script = wait.until(EC.presence_of_element_located((By.XPATH, "//script[@type='application/ld+json']")))
         json_data = json.loads(script.get_attribute('innerHTML'))
         logger.info("JSON data retrieved successfully.")
-
+        
         # Initialize variables to store the extracted data
         menu_items = {}
         full_address = None
-
+        
         # Extract menu items if available
         if 'hasMenu' in json_data:
             logger.info("Found 'hasMenu' in JSON data.")
@@ -187,45 +161,71 @@ def scrape_restaurant_data(driver, restaurant_name, location, carousel = False):
                     price = item.get('offers', {}).get('price')
                     if name and price:
                         menu_items[name] = price
-
+        
         # Extract the full address if available
         if "address" in json_data:
             logger.info("Found 'address' in JSON data.")
             address_parts = [json_data['address'].get(part) for part in ['streetAddress', 'addressLocality', 'addressRegion', 'postalCode', 'addressCountry']]
             full_address = ', '.join(filter(None, address_parts))
-
-        #Extracting DISTANCE and RATINGS 
+        
+        # Extracting DISTANCE and RATINGS 
         info_elems = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//span[@data-testid='rich-text']")))
-
+        
         # Check if there are at least four bar elements
         if len(info_elems) >= 4:
-           
             ratings_elem = info_elems[0]
             rating = ratings_elem.text
             logger.info(f"Found rating: {rating}")
-
+            
             no_ratings_elem = info_elems[1]
             no_ratings_txt = no_ratings_elem.text
-            no_ratings = re.search('(\d{1,3}\+?)', no_ratings_txt).group(1)
-            logger.info(f"Found number of ratings: {no_ratings}")
-
+            no_ratings_match = re.search(r'(\d{1,3}\+?)', no_ratings_txt)
+            if no_ratings_match:
+                no_ratings = no_ratings_match.group(1)
+                logger.info(f"Found number of ratings: {no_ratings}")
+            else:
+                no_ratings = None
+                logger.info("No ratings found.")
+            
             distance_elem = info_elems[3]  # This will get the fourth element in the list
             distance = distance_elem.text
             logger.info(f"Found distance: {distance}")
-
         else:
             logger.info("Not enough rating elements found")
             rating = None   
             distance = None
             no_ratings = None
-
+        
+        # Scrape item names and prices
+        try:
+            item_elements = driver.find_elements(By.XPATH, "//span[@data-testid='rich-text']")
+            items = []
+            prices = []
+            
+            for elem in item_elements:
+                text = elem.text.strip()
+                if text.startswith('$'):
+                    prices.append(text)
+                else:
+                    items.append(text)
+            
+            if len(items) == len(prices):
+                for item, price in zip(items, prices):
+                    menu_items[item] = price
+                    logger.info(f"Found item: {item} with price: {price}")
+            else:
+                logger.warning("Mismatch between the number of items and prices.")
+        
+        except Exception as e:
+            logger.error(f"Error while scraping menu items and prices: {e}")
+        
         # Update item_data with collected information
         item_data['menu'] = menu_items
         item_data['location'] = full_address
-        item_data['rating'] = rating #make sure you assign the rating text as rating
+        item_data['rating'] = rating  # make sure you assign the rating text as rating
         item_data['number of ratings'] = no_ratings
         item_data['distance'] = distance
-
+        
         try:
             # Attempt to find the close button using its aria-label
             wait_2 = WebDriverWait(driver, 1)
@@ -236,12 +236,12 @@ def scrape_restaurant_data(driver, restaurant_name, location, carousel = False):
             driver.back()
         except:
             logger.info(" ")
-
+    
     except Exception as e:
         logger.error(f"Error while scraping data: {e}")
     
-    
     return item_data
+
 
 def get_carousel_items(driver, restaurant, location):
     if find_search_box_and_enter_query(driver, restaurant, restaurant_or_location="RESTAURANT"):
