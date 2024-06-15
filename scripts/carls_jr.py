996bd38415c853bed90f4a785461822e318aa179
Segun Ajibola <nguyena607@berkeley.edu>
2024-06-15 14:07:01 -0700

    calrs jr script

--- scripts/carls_jr.py
+++ scripts/carls_jr.py
@@ -18,10 +18,10 @@ from selenium.webdriver.common.action_chains import ActionChains
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-FILE_PATH = "raw_prices_carlsjr_non_ca_05162024.csv"
+FILE_PATH = "carlsjr_missing_rnd1.csv"
 # Use your own executable_path (download from https://chromedriver.chromium.org/).
-#CHROMEDRIVER_PATH = "/Users/alyssanguyen/Downloads/chromedriver-mac-arm64/chromedriver"
-CHROMEDRIVER_PATH = "/Users/sakshikolli/Downloads/chromedriver-mac-x64/chromedriver"
+CHROMEDRIVER_PATH = "/Users/alyssanguyen/Downloads/chromedriver-mac-arm64/chromedriver"
+#CHROMEDRIVER_PATH = "/Users/sakshikolli/Downloads/chromedriver-mac-x64/chromedriver"
 
 def clear_existing_data(file_path):
     try:
@@ -39,59 +39,136 @@ def setup_driver():
     driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
 
+def click_next_button(driver):
+    try:
+        next_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "div.swiper-button-next"))
+        )
+        driver.execute_script("arguments[0].click();", next_button)
+        print("Clicked on the next button with the SVG icon using JavaScript.")
+        return True
+    except Exception as e:
+        print(f"Failed to click on the next button with the SVG icon: {e}")
+        return False
+
+def click_charbroiled_burgers(driver, retries=3):
+    for attempt in range(retries):
+        try:
+            # Try to click on the 'Charbroiled Burgers' button
+            search_box4 = WebDriverWait(driver, 5).until(
+                EC.element_to_be_clickable((By.XPATH, './/button[contains(text(),"Charbroiled Burgers")]'))
+            )
+            search_box4.click()
+            print("Clicked on 'Charbroiled Burgers'.")
+            return True
+        except Exception as e:
+            print(f"Attempt {attempt+1}: Failed to find 'Charbroiled Burgers' button, clicking on the next button instead.")
+            # Click the next button with the SVG icon
+            if not click_next_button(driver):
+                continue
+            time.sleep(3)
+    print("Failed to click on 'Charbroiled Burgers' after multiple attempts.")
+    return False
+
+clear_existing_data(FILE_PATH)
+
 carls_jr_locations = [
-    '1700 W State St, Boise, ID, 83702, US',
-    '493 N Milwaukee St, Boise, ID, 83704, US',
-    '4994 E 41St, Tulsa, OK, 74135, US',
-    '1039 W University Ave, Georgetown, TX, 78628, US',
-    '925 w Antelope Drive, Layton, UT, 84041, US',
-    '2118 W 1700 S SYRACUSE, UT 84075, US',
-    '582 N MAIN STREET CLEARFIELD, UT 84015, US',
-    '1155 W RIVERDALE RD UNIT A RIVERDALE, UT 84405',
-    '5722 S 49TH WEST AVE TULSA, OK 74107'
+    '1001 Veterans Blvd, Redwood City, CA, 94063, US',
+    '3131 Crow Canyon Pl, San Ramon, CA, 94583, US',
+    '16101 N. Harbor Blvd, Fountain Valley, CA, 92708, US',
+    '16565 Sierra Lakes Pkwy, Fontana, CA, 92336, US',
+    '939 W. Charter Way, Stockton, CA, 95206, US',
+    '3370 La Sierra Ave, Riverside, CA, 92503, US',
+    '14454 Valley Blvd, Fontana, CA, 92335, US',
+    '1670 Pacific Coast Highway, Long Beach, CA, 90810, US',
+    '4595 Century Boulevard, Pittsburg, CA, 94565, US',
+    '2280 Arden Way, Sacramento, CA, 95825, US',
+    '7432 Pacific Ave, Stockton, CA, 95207, US',
+    '222 N. Euclid Ave, Fullerton, CA, 92832, US',
+    '2400 White Ln, Bakersfield, CA, 93304, US',
+    '1259 W Carson St, Torrance, CA, 90502, US',
+    '46637 Mission Boulevard, Fremont, CA, 94539, US',
+    '2110 W 7Th St, Los Angeles, CA, 90057, US',
+    '1095 Oakland Road, San Jose, CA, 95112, US',
+    '1588 N 1st St, Fresno, CA, 93703, US',
+    '5670 Thornton Avenue, Newark, CA, 94560',
+    '2903 Burgener Blvd, San Diego, CA, 92110, US'
 ]
 
-driver = setup_driver()
 for idx, location in enumerate(carls_jr_locations):
-    driver.get('https://order.carlsjr.com/?utm_medium=cpc&utm_source=google&utm_campaign=carls-jr_n_807-sf-ca_ggl_sem_branded_general_eng&utm_content=carls%20jr&utm_term=carl%27s%20jr&device=c&gad_source=1&gclid=Cj0KCQjwqpSwBhClARIsADlZ_TmNvYfPIiDHUv6GbAPKlm0pXEJEe1deauPhqf90B9d8rr9E9CkG_LUaAjz6EALw_wcB&gclsrc=aw.ds')
+    driver = setup_driver()  # Create a new driver instance for each location
+    
+    try:
+        driver.get('https://order.carlsjr.com/?utm_medium=cpc&utm_source=google&utm_campaign=carls-jr_n_807-sf-ca_ggl_sem_branded_general_eng&utm_content=carls%20jr&utm_term=carl%27s%20jr&device=c&gad_source=1&gclid=Cj0KCQjwqpSwBhClARIsADlZ_TmNvYfPIiDHUv6GbAPKlm0pXEJEe1deauPhqf90B9d8rr9E9CkG_LUaAjz6EALw_wcB&gclsrc=aw.ds')
+        
+        # Click on popup screen
+        continue_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))
+        )
+        continue_button.click()
+        print("Clicked on 'Continue to Site' button successfully.")
+        time.sleep(3)
 
-    search_box = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/ion-app/ion-router-outlet/app-custom-location-finder/ion-content/div/main/div/div[1]/header/form/div/app-custom-location-search/div/div[2]/input"))
-    )
-    search_box.clear()
-    search_box.click()
-    search_box.send_keys(location)
+        # Type in the address
+        search_box = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/ion-app/ion-router-outlet/app-custom-location-finder/ion-content/div/main/div/div[1]/header/form/div/app-custom-location-search/div/div[2]/input"))
+        )
+        search_box.clear()
+        search_box.click()
+        search_box.send_keys(location)
+        print(f"Typed in address: {location}")
+        time.sleep(3)
+        
+        # Click on the first suggested address
+        li_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//li[@class='ng-star-inserted']//button"))
+        )
+        li_element.click()
+        print("Clicked on the suggested address successfully.")
+        time.sleep(3)
+        
+        # Click on the 'Start Order' button
+        button = driver.find_element(By.XPATH, "//button[@class='btn--primary' and contains(text(), 'Start Order')]")
+        ActionChains(driver).move_to_element(button).click().perform()
+        print("Clicked on 'Start Order' button successfully using ActionChains.")
+        
+        time.sleep(4)
 
-    ActionChains(driver).move_to_element(search_box).move_by_offset(0, -5).perform()
+        # Extract actual address from <a> tag
+        address_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "a.c-menu__order-details__location"))
+        )
+    
+        # Extract the text content of the address element
+        actual_address = address_element.text
+        
+        print(f"Menu Address: {actual_address}")
+    
+        # Try to click on the 'Charbroiled Burgers' button or click the next button if it fails
+        if not click_charbroiled_burgers(driver):
+            print("Skipping this location due to failure in finding the 'Charbroiled Burgers' button.")
+            continue
+             
+        # Extract data and store in CSV
+        regex_pattern = 'class="product-card__name">(.*?)<\/div>.*?class="product-card__cost-calories">\$([\d\.]+).*?<\/span><!---->\s([\d,]*)'
+        string_burgers = driver.page_source
+        result_burgers = re.findall(regex_pattern, string_burgers)
+            
+        data = pd.DataFrame(result_burgers, columns=['menu_item', 'menu_item_price', 'menu_item_calories'])
+        data['restaurant_address'] = location
+        data['inputted_address'] = actual_address
+            
+        if idx == 0:
+            data.to_csv(FILE_PATH, index=False)
+        else:
+            data1 = pd.read_csv(FILE_PATH)
+            df = pd.concat([data1, data])
+            df.to_csv(FILE_PATH, index=False)
+    
+    except Exception as e:
+        logger.error(f"Error processing location '{location}': {e}")
+    
+    finally:
+        driver.quit()  # Quit the driver instance after processing each location
 
-    search_box2 = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/ion-app/ion-router-outlet/app-custom-location-finder/ion-content/div/main/div/div[1]/header/form/div/app-custom-location-search/div/div[3]/ul/li[1]/button'))
-    )
-    search_box2.click()
-    time.sleep(3)
-
-    search_box3 = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/ion-app/ion-router-outlet/app-custom-location-finder/ion-content/div/main/div/div[1]/div/div[2]/swiper/div/div[1]/app-custom-location-card[1]/div/div/div[3]/div/button')))
-    search_box3.click()
-    time.sleep(5)
-
-    search_box4 = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable((By.XPATH, './/button[contains(text(),"Charbroiled Burgers")]')))
-    search_box4.click()
-    time.sleep(3)
-
-    regex_pattern = 'class="product-card__name">(.*?)<\/div>.*?class="product-card__cost-calories">\$([\d\.]+).*?<\/span><!---->\s([\d,]*)'
-
-    string_burgers = driver.page_source
-    result_burgers = re.findall(regex_pattern, string_burgers)
-
-    data = pd.DataFrame(result_burgers, columns = ['menu_item', 'menu_item_price', 'menu_item_calories'])
-    data['restaurant_address'] = location
-    if idx == 0:
-        data.to_csv(FILE_PATH, index=False)
-    else:
-        data1 = pd.read_csv(FILE_PATH)
-        df = pd.concat([data1, data])
-        df.to_csv(FILE_PATH, index = False)
-
-
+print("Script execution completed.")
